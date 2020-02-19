63a341c81546146513444656a1d75a715e4b7404
RODRIGO BARRETO <rodrigo.barreto1@senacsp.edu.br>
2020-02-19 17:24:09 -0300

    Creating get from rest

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require('express')
 const app = express()
 
+let users={
+    1:{
+        id: '1',
+        username: 'barreto'
+    },
+    2:{
+        id: '2',
+        username: 'rodrigo'
+    }
+}
+
 app.listen(3000, function(){
     console.log('Hello World')
+})
+
+app.get('/', (req, res)=>{
+    //res.send('Hello World!')
+    return res.send(Object.values(users))
 })
